bbb1f1d0ce37d437f209adf1db4de95ce3b26a9d
florine.pawlowski <florine.pawlowski.etu@univ-lille.fr>
2025-11-04 17:00:34 +0100

    feat: add User Controller

---
+++ auth/src/main/java/com/alom/auth/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.alom.auth.controller;
+
+import com.alom.auth.dto.AuthRequestDTO;
+import com.alom.auth.dto.AuthResponseDTO;
+import com.alom.auth.dto.TokenValidationRequestDTO;
+import com.alom.auth.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public AuthResponseDTO register(@Valid @RequestBody AuthRequestDTO request) {
+        return authService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponseDTO login(@Valid @RequestBody AuthRequestDTO request) {
+        return authService.login(request);
+    }
+
+    @PostMapping("/token")
+    public Boolean validateToken(@Valid @RequestBody TokenValidationRequestDTO request) {
+        return authService.isTokenValid(request);
+    }
+}
