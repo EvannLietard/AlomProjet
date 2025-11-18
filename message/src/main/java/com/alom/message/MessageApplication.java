5529b3c8fad7b138be92d05e74913af92c4cd00d
evann.lietard <evann.lietard.etu@univ-lille.fr>
2025-11-18 17:23:38 +0100

    feat(message): activation du scheduling pour la découverte dynamique

--- message/src/main/java/com/alom/message/MessageApplication.java
+++ message/src/main/java/com/alom/message/MessageApplication.java
@@ -2,8 +2,10 @@ package com.alom.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class MessageApplication {
 
 	public static void main(String[] args) {
