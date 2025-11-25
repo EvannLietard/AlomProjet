bd37129b53082059b1433839ae6036bb15b85020
florine.pawlowski <florine.pawlowski.etu@univ-lille.fr>
2025-11-25 13:49:43 +0100

    feat: forget some files

---
+++ push/src/main/java/com/alom/push/client/AuthClient.java
@@ -0,0 +1,18 @@
+package com.alom.push.client;
+
+import com.alom.push.dto.TokenValidationRequestDTO;
+import com.alom.push.dto.TokenValidationResponseDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@FeignClient(name = "auth-service", url = "${auth.service.url:http://localhost:8081}")
+public interface AuthClient {
+
+    @PostMapping("/auth/token")
+    Boolean validateToken(@RequestBody TokenValidationRequestDTO request);
+
+    @PostMapping("/auth/user")
+    TokenValidationResponseDTO getUserByToken(@RequestBody TokenValidationRequestDTO request);
+}
+
